5293410dbb7956c3e8d339b50917976f9b25aef9
edpaulsen <ed@paulsenhome.com>
2021-05-26 21:25:14 -0500

    end of section 7

--- src/App.js
+++ src/App.js
@@ -7,42 +7,57 @@ import {listLists} from './graphql/queries';
 import 'semantic-ui-css/semantic.min.css';
 import MainHeader from './components/headers/MainHeaders';
 import Lists from './components/Lists/Lists';
-import {Button, Container, Icon, Modal} from "semantic-ui-react";
+import {Button, Container, Form, Icon, Modal} from "semantic-ui-react";
 Amplify.configure(awsConfig);
 
 function App() {
     const [lists, setLists] = useState([]);
-    const [isModalOpen, setIsModalOpen] = useState(false)
-    async function fetchList() {
-        const { data } = await API.graphql(graphqlOperation(listLists));
-        setLists(data.listLists.items);
-    }
-    useEffect(() => {
-        fetchList();
-    },[]);
+    const [isModalOpen, setIsModalOpen] = useState(true)
 
-    function openModal() {
-        setIsModalOpen(true);
-    }
+        async function fetchList() {
+            const {data} = await API.graphql(graphqlOperation(listLists));
+            setLists(data.listLists.items);
+            console.log(data);
+        }
 
+        useEffect(() => {
+            fetchList();
+        }, []);
+
+        function toggleModal(shouldOpen) {
+            setIsModalOpen(shouldOpen);
+    }
   return (
       <AmplifyAuthenticator>
-          <AmplifySignOut />
-          <Button className='floatingButtom' onClick={openModal}>
-              <Icon name='plus' className='floatingButtom_icon'/>
-          </Button>
-              <Container>
+
+              <Container style={{height: '100vh'}}>
+                  <AmplifySignOut />
+                  <Button className='floatingButton' onClick={() => toggleModal(true)}>
+                      <Icon name='plus' className='floatingButton_icon'/>
+                  </Button>
                 <div className='App'>
                     <MainHeader />
                     <Lists lists={lists} />
                 </div>
               </Container>
-          <Modal open={isModalOpen} dimmer='blurring'>
+          <Modal open={isModalOpen} >
               <Modal.Header>Create Your List</Modal.Header>
-              <Modal.Content>Form to be added later</Modal.Content>
+              <Modal.Content>
+                  <Form>
+                      <Form.Input
+                          error={true ? false : {content: "Please add a name to your list"}}
+                          label=" Title"
+                          placeholder="My Pretty List">
+                      </Form.Input>
+                      <Form.TextArea
+                          label='Description'
+                          placeholder="Things that my Pretty list is about">
+                      </Form.TextArea>
+                  </Form>
+              </Modal.Content>
               <Modal.Actions>
-                  <Button negative>Cancel</Button>
-                  <Button positive>Save</Button>
+                  <Button negative onClick={() => toggleModal(false)} >Cancel</Button>
+                  <Button positive onClick={() => toggleModal(false)} >Save</Button>
               </Modal.Actions>
           </Modal>
           </AmplifyAuthenticator>
